4994c92e2f6241c043b227e5e8ba8bb40637e95c
Tejaswi-17 <73527680+Tejaswi-17@users.noreply.github.com>
2021-11-14 15:30:26 +0530

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,6 +3,8 @@ import MainNavbar  from "../MainNavbar/MainNavbar"
 import ScholarShipStatus from "../ScholarShipStatus/ScholarShipStatus"
 import FieldsList from "../FieldsList/FieldsList"
 import "./index.css"
+import ContactUs from "../ContactUs/ContactUs"
+import Chatbox from "../Chatbox/Chatbox"
 const Home=props=>{
     const data_object = props.location.state
     return(<div className="main-home"><MainNavbar data_name={data_object[0]["firstname"]}/><FieldsList data={data_object[0]["fileds_list"]}/><ScholarShipStatus data={data_object[0]["scholar"]}/>
